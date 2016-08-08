da7d6a9984c2feb5f15a749609be1b6c08c38473
huberchrigu <huber.chrigu@gmail.com>
2016-08-08 18:33:08 +0200

    Initial commit

---
+++ browser.d.ts
@@ -0,0 +1,23 @@
+nterface ngStomp {
+        sock:any;
+        stomp:any;
+        debug:any;
+        off: any;
+
+        setDebug:(callback:Function)=> void;
+
+        connect: (endpoint:string, headers?:Headers)=> angular.IHttpPromise<any>;
+
+        disconnect: (callback:()=>void) => angular.IHttpPromise<any>;
+
+        subscribe: (destination:string, callback:(payload:string, headers:Headers, res:Function)=>void, headers?:Headers, scope?:any) => any;
+
+        unsubscribe: () => any;
+
+        send: (destination:string, body:any, headers:Headers)=> any;
+
+    }
+
+     interface Headers {
+        [key: string]: any;
+    }
